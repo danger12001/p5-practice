181b8a0899a386a6ce2ea2791342b33a0e184b77
danger12001 <paul@projectcodex.co>
2016-09-17 17:02:57 +0200

    experimental slider

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
-
+var data;
 var x;
 var y;
+var strokeColor;
 function setup() {
+  data = createDiv('');
   createCanvas(windowWidth, windowHeight);
   x = createDiv('X: ' + 0);
   y = createDiv('Y: ' + 0);
+  x.parent(data);
+  y.parent(data);
   x.position(50, 50);
   y.position(50, 80);
-
+var slider = createSlider(100, 255 , 0);
+  slider.position(50, 100);
+  slider.parent(data);
+  strokeColor = slider.value();
 }
+
 function mouseMoved() {
   x.remove();
   y.remove();
@@ -20,7 +28,9 @@ function mouseMoved() {
 }
 
 function mousePressed(){
-ellipse(mouseX, mouseY, 25, 25);
+
+    ellipse(mouseX, mouseY, 10, 10).fill(strokeColor);
+
 x.remove();
 y.remove();
 x = createDiv('X: ' + mouseX);
@@ -32,7 +42,7 @@ y.position(50, 80);
 }
 
 function mouseDragged(){
-ellipse(mouseX, mouseY, 25, 25);
+ellipse(mouseX, mouseY, 10, 10);
 x.remove();
 y.remove();
 x = createDiv('X: ' + mouseX);
